c549d8bf8f97bf1c4afbe60dc12907503aaed01f
Philip Jönsson <philip.jonsson@dignitana.se>
2017-06-01 16:35:43 +0200

    Starting AddVoucher window

--- GUI/AddVoucher.cs
+++ GUI/AddVoucher.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FolkBok.GUI
+namespace FolkBok
 {
     public partial class AddVoucher : Form
     {
--- GUI/MainWindow.cs
+++ GUI/MainWindow.cs
@@ -19,6 +19,8 @@ namespace FolkBok
         {
             InitializeComponent();
 
+            AddVoucher addVoucher = new AddVoucher();
+            addVoucher.ShowDialog();
             AddInvoice addInvoice = new AddInvoice();
             addInvoice.ShowDialog();
 
